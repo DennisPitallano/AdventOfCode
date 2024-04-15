b9619cad329e188a8820707c5f2b6b2ff5fb3fc1
Dennis Pitallano <dpitallano@ucpm.com>
2024-04-16 05:23:17 +0800

    --- Day 4: The Ideal Stocking Stuffer ---

---
+++ 2015/2015.Day.4/Calculate.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+namespace _2015.Day._4
+{
+    public static class Calculate
+    {
+        public static int FindLowestNumber(string input,string prefix ="00000")
+        {
+            var number = 0;
+            while (true)
+            {
+                var hash = CalculateMd5Hash(input + number);
+                if (hash.StartsWith(prefix))
+                {
+                    break;
+                }
+                number++;
+            }
+            return number;
+        }
+
+        private static string CalculateMd5Hash(string input)
+        {
+            using var md5 = MD5.Create();
+            var inputBytes = Encoding.ASCII.GetBytes(input);
+            var hashBytes = md5.ComputeHash(inputBytes);
+            var sb = new StringBuilder();
+            for (var i = 0; i < hashBytes.Length; i++)
+            {
+                sb.Append(hashBytes[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ 2015/2015.Day.4/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using _2015.Day._4;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine(Calculate.FindLowestNumber("ckczppom"));
+Console.WriteLine(Calculate.FindLowestNumber("ckczppom","000000"));
